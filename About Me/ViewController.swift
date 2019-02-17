0ef65b7fb7a71f02f775feb69b919aa34359497b
Alex Perse <alexanderperse@gmail.com>
2019-02-17 18:07:04 -0500

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Alex Perse on 2/17/19.
+//  Copyright © 2019 Alex Perse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
