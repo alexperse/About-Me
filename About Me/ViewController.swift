54333d395506f24d3bfb03a5c63ab1f2a1b7a1dd
Alex Perse <alexanderperse@gmail.com>
2019-02-17 18:40:25 -0500

    About Me App
    About Me App from Lambda School's iOS precourse

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func introduceMeButton(_ sender: Any) {
+        let name = "Alex"
+        let homeTown = "Atlanta"
+        var favoriteColor = "Blue"
+        var favoriteFood = "Sushi"
+        print(name)
+        print(homeTown)
+        print(favoriteColor)
+        print(favoriteFood)
+        mainTextView.text = ("My name is  \(name). My hometown is \(homeTown). My favorite color is \(favoriteColor). My favorite food is \(favoriteFood).")
+    }
+    
+    @IBOutlet weak var mainTextView: UITextView!
 }
 
